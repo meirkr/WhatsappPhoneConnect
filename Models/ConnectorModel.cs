0cddad4ae7cee1c61079904a5ea5baddd0daedf6
Meir Kriheli <meirkr@gmail.com>
2020-08-06 01:26:55 +0300

    Add project files.

---
+++ Models/ConnectorModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatsappPhoneConnect.Models
+{
+    public class ConnectorModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "חסר מספר טלפון")] 
+        public string PhoneNumber { get; set; }
+    }
+}
