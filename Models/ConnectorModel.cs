5966ea4d23c5e3995f725e785bd27f9f8f2271af
Meir Kriheli <meirkr@gmail.com>
2020-09-26 23:45:24 +0300

    use number inpt box for phone number

--- Models/ConnectorModel.cs
+++ Models/ConnectorModel.cs
@@ -5,6 +5,6 @@ namespace WhatsappPhoneConnect.Models
     public class ConnectorModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "חסר מספר טלפון")] 
-        public string PhoneNumber { get; set; }
+        public int? PhoneNumber { get; set; }
     }
 }
